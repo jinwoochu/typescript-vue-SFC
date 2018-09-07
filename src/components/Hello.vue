e42581ff3bd0112add78c172933430f2086b75da
jinwoochu <cjw0672@naver.com>
2018-09-07 17:03:21 +0900

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="greeting">Hello {{name}}{{exclamationMarks}}</div>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>  
+</template>
+
+<style scoped>
+.greeting {
+    font-size: 50px;
+}
+</style>
+
+<script lang="ts">
+// import Hello from './Hello'
+// export default Hello
+
+import Vue from "vue"
+import { Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Hello extends Vue {
+  // props
+  @Prop() name: any
+  @Prop() initialEnthusiasm: any
+
+  // data
+
+  enthusiasm: number = this.initialEnthusiasm
+
+  // computed
+
+  get exclamationMarks(): string {
+    return Array(this.enthusiasm + 1).join('!')
+  }
+
+  // methods
+
+  increment(): void {
+    this.enthusiasm++;
+  }
+
+  decrement(): void {
+    if (this.enthusiasm > 1) {
+      this.enthusiasm--
+    }
+  }
+}
+</script>
+
+
